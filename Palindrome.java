78ffda175f9852662c867f275d2ec23700d9391a
Abhisiva5 <abhilasasivaraman.001@gmail.com>
2022-12-01 10:44:28 +0530

    Initial commit

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String str,rev ="";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String to check:");
+        str = sc.nextLine();
+        int n = str.length();
+        for(int i = n-1;i>=0;i--) {
+            
+        	rev = rev+str.charAt(i);
+        }
+        
+        if(str.equals(rev)) {
+        	System.out.println(str+" is a palindrome");
+        }
+        else {
+        	System.out.println(str+" is not a palindrome");
+        }
+        }
+	}
